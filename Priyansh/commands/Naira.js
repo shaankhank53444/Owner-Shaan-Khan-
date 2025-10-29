d808f3f7f5907725b6bb65e44a2e230e053799d5
Shaan Personal bat <b8465391@gmail.com>
2025-10-29 08:35:25 +0500

    Create Naira.js

---
+++ Priyansh/commands/Naira.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "Naira",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "uzairrajput", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "npxs5",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("naira")==0 || event.body.indexOf("Naira")==0 || event.body.indexOf("NAIRA")==0 || event.body.indexOf("naira")==0) {
+    var msg = {
+        body: ``,
+        attachment: fs.createReadStream(__dirname + `/uzair/Naira.mp3`)
+      }
+      api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("❣️", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+    }
