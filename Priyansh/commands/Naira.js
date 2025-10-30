5e5a5cff18a21f748d5cb50bad4337dfe91ac738
Shaan Personal bat <b8465391@gmail.com>
2025-10-30 10:05:39 +0500

    Update Naira.js

--- Priyansh/commands/Naira.js
+++ Priyansh/commands/Naira.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "Naira",
+        name: "shona",
     version: "1.0.1",
-  hasPermssion: 0,
-  credits: "uzairrajput", 
-  description: "hihihihi",
-  commandCategory: "no prefix",
-  usages: "npxs5",
+        hasPermssion: 0,
+        credits: "Shaan", 
+        description: "hihihihi",
+        commandCategory: "no prefix",
+        usages: "npxs5",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("naira")==0 || event.body.indexOf("Naira")==0 || event.body.indexOf("NAIRA")==0 || event.body.indexOf("naira")==0) {
-    var msg = {
-        body: ``,
-        attachment: fs.createReadStream(__dirname + `/uzair/Naira.mp3`)
-      }
-      api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("❣️", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        if (event.body.indexOf("naira")==0 || event.body.indexOf("NAIRA")==0 || event.body.indexOf("@naira")==0 || event.body.indexOf("Naira")==0) {
+                var msg = {
+                                body: "𝐍𝐀𝐈𝐑𝐀 𝐒𝐇𝐀𝐀𝐍 𝐁𝐎𝐒𝐒 𝐊𝐈 𝐖𝐈𝐅𝐄 𝐇𝐀𝐈 𝐌𝐄𝐍𝐓𝐈𝐎𝐍 𝐍𝐀 𝐊𝐀𝐑𝐎𝐎 😏😏",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/Naira.mp3`)
+                        }
+                        api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-    }
+        }
