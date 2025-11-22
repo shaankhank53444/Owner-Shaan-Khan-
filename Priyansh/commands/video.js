eb03b2551256be75623b43760ec3d726d02f9769
Shaan Personal bat <b8465391@gmail.com>
2025-11-22 08:59:12 +0500

    Update video.js

--- Priyansh/commands/video.js
+++ Priyansh/commands/video.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports.config = {
-  name: "video",
+  name: "Video",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "ArYAN",
