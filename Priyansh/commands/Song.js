070d1fd0aad9df1a9a6f8c351f50661900e342fe
Shaan Personal bat <b8465391@gmail.com>
2025-11-29 15:42:04 +0500

    Update Song.js

--- Priyansh/commands/Song.js
+++ Priyansh/commands/Song.js
@@ -33,7 +33,7 @@ function getVideoID(url) {
 }
 
 module.exports.config = {
-    name: "yt",
+    name: "song",
     version: "1.1.0",
     credits: "ARIF-BABU", // 🔐 DO NOT CHANGE
     hasPermssion: 0,
