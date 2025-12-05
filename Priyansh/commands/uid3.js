40328c0b8ce115dec4e40d63e896bc36b054a423
Shaan Personal bat <b8465391@gmail.com>
2025-12-05 12:03:57 +0500

    Update uid3.js

--- Priyansh/commands/uid3.js
+++ Priyansh/commands/uid3.js
@@ -1,44 +1,85 @@
 module.exports.config = {
-    name: "uid3",
-    version: "3.0.0",
-    hasPermssion: 0,
-    credits: "SHAAN-KHAN", // DO NOT CHANGE
-    description: "Send FB Contact Card Style",
-    commandCategory: "Tools",
-    cooldowns: 5
+  name: "uid3",
+  version: "6.0.0",
+  hasPermssion: 0,
+  credits: "ARIF-BABU",
+  description: "Generate stylish Facebook info card with circular DP",
+  commandCategory: "Tools",
+  cooldowns: 5
 };
 
-module.exports.run = async function({ api, event }) {
+module.exports.run = async function ({ api, event }) {
 
-    // 🔒 Credit Lock Protection
-    if (module.exports.config.credits !== "SHAAN-KHAN") {
-        return api.sendMessage(
-            "⚠ SECURITY ALERT ⚠\n❌ Unauthorized modification detected!\nCredits change is not allowed.",
-            event.threadID,
-            event.messageID
-        );
-    }
+  const fs = global.nodemodule["fs-extra"];
+  const request = global.nodemodule["request"];
+  const { createCanvas, loadImage } = require("canvas");
 
-    let uid, name;
+  let uid, name;
 
-    if (Object.keys(event.mentions).length === 0) {  
-        uid = event.senderID;  
-        name = "You";  
-    } else {  
-        uid = Object.keys(event.mentions)[0];  
-        name = event.mentions[uid].replace("@", "");  
-    }  
+  if (Object.keys(event.mentions).length > 0) {
+    uid = Object.keys(event.mentions)[0];
+    name = event.mentions[uid].replace("@", "");
+  } else {
+    uid = event.senderID;
+    name = "Facebook User";
+  }
 
-    const fbProfile = `https://www.facebook.com/profile.php?id=${uid}`;  
+  const dpURL = `https://graph.facebook.com/${uid}/picture?height=600&width=600&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+  const filePath = __dirname + `/cache/circle_${uid}.png`;
 
-    const msg = 
-`╭━━━━━━━━━━━━╮
-│ ✨ Name: ${name}
-│ 🆔 UID: ${uid}
-│━━━━━━━━━━━━━
-│ 🌐 Facebook Profile:
-│ ${fbProfile}
-╰━━━━━━━━━━━━━╯`;
+  await new Promise(resolve =>
+    request(dpURL)
+      .pipe(fs.createWriteStream(filePath))
+      .on("close", resolve)
+  );
 
-    return api.sendMessage(msg, event.threadID, event.messageID);
+  const img = await loadImage(filePath);
+
+  const canvas = createCanvas(512, 512);
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, 512, 512);
+
+  ctx.beginPath();
+  ctx.arc(256, 256, 250, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+
+  ctx.drawImage(img, 0, 0, 512, 512);
+
+  const finalPath = __dirname + `/cache/finaldp_${uid}.png`;
+  fs.writeFileSync(finalPath, canvas.toBuffer());
+
+  const moment = require("moment-timezone");
+  moment.tz.setDefault("Asia/Dhaka");
+
+  const date = moment().format("DD/MM/YYYY");
+  const time = moment().format("hh:mm:ss A");
+  const day = moment().format("dddd");
+
+  let msg =
+`━━━━━━━━━━━━━━━━━━
+🤖 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 🎉
+━━━━━━━━━━━━━━━━━━
+📅 Date: ${date}
+🕒 Time: ${time}
+📆 Day: ${day}
+
+👤 Name: ${name}
+🆔 UID: ${uid}
+━━━━━━━━━━━━━━━━━━`;
+
+  api.sendMessage(
+    {
+      body: msg,
+      attachment: fs.createReadStream(finalPath)
+    },
+    event.threadID,
+    () => {
+      fs.unlinkSync(filePath);
+      fs.unlinkSync(finalPath);
+    },
+    event.messageID
+  );
 };
