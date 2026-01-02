c1ae221d65707e626919f5e8f0e2d378f0305cc4
Shaan Personal bat <b8465391@gmail.com>
2026-01-02 12:12:11 +0500

    Update unsend.js

--- Priyansh/commands/unsend.js
+++ Priyansh/commands/unsend.js
@@ -1,9 +1,9 @@
 module.exports.config = {
     name: "unsend",
-    version: "1.0.3",
+    version: "1.0.5",
     hasPermssion: 0,
     credits: "Shaan Khan",
-    description: "Bot ke message par 😾 react karein ya reply karke unsend likhein",
+    description: "Bot ke message par 😾 emoji react karein ya reply karke unsend likhein",
     commandCategory: "system",
     usages: "unsend",
     cooldowns: 0
@@ -16,18 +16,23 @@ module.exports.languages = {
     },
     "en": {
         "returnCant": "Main sirf apne messages unsend kar sakta hoon!",
-        "missingReply": "Mere jis message ko hatana hai uspar 😾 react karein."
+        "missingReply": "Bot ke message par 😾 react karein unsend karne ke liye."
     }
 }
 
 module.exports.handleReaction = async function({ api, event }) {
+    // Check karega ki reaction dene wala bot khud nahi hai aur emoji 😾 hai
     if (event.reaction == "😾") {
-        api.unsendMessage(event.messageID);
+        return api.unsendMessage(event.messageID);
     }
 };
 
-module.exports.run = function({ api, event, getText }) {
-    if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-    if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+module.exports.run = async function({ api, event, getText }) {
+    if (event.type != "message_reply") {
+        return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+    }
+    if (event.messageReply.senderID != api.getCurrentUserID()) {
+        return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+    }
     return api.unsendMessage(event.messageReply.messageID);
 };
