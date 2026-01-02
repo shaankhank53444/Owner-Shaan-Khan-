4690ecb4f35158c029964e15fa77efc183b21caa
Shaan Personal bat <b8465391@gmail.com>
2026-01-02 12:04:34 +0500

    Update unsend.js

--- Priyansh/commands/unsend.js
+++ Priyansh/commands/unsend.js
@@ -1,27 +1,33 @@
 module.exports.config = {
-	name: "unsend",
-	version: "1.0.1",
-	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Gỡ tin nhắn của bot",
-	commandCategory: "system",
-	usages: "unsend",
-	cooldowns: 0
+    name: "unsend",
+    version: "1.0.2",
+    hasPermssion: 0,
+    credits: "Shaan Khan",
+    description: "Bot ke message par ❤️ react karein ya reply karke unsend likhein",
+    commandCategory: "system",
+    usages: "unsend",
+    cooldowns: 0
 };
 
 module.exports.languages = {
-	"vi": {
-		"returnCant": "Không thể gỡ tin nhắn của người khác.",
-		"missingReply": "Hãy reply tin nhắn cần gỡ."
-	},
-	"en": {
-		"returnCant": "Kisi Aur Ka Msg M Kese Unsend Karu.",
-		"missingReply": "Mere Jis Msg ko Unsend Karna Hai Usme Reply Karke Likkho."
-	}
+    "vi": {
+        "returnCant": "Không thể gỡ tin nhắn của người khác.",
+        "missingReply": "Hãy reply tin nhắn cần gỡ."
+    },
+    "en": {
+        "returnCant": "Main sirf apne messages unsend kar sakta hoon!",
+        "missingReply": "Mere jis message ko hatana hai, uspar reply karke unsend likhein ya ❤️ emoji react karein."
+    }
 }
 
+module.exports.handleReaction = async function({ api, event }) {
+    if (event.reaction == "❤️") {
+        api.unsendMessage(event.messageID);
+    }
+};
+
 module.exports.run = function({ api, event, getText }) {
-	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
-	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-	return api.unsendMessage(event.messageReply.messageID);
-	}
+    if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+    if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+    return api.unsendMessage(event.messageReply.messageID);
+};
